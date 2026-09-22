a647468ed7c3e6d7911cbef4d8ad6a2b37b27ec3
Srijani-Guchhait <srijaniguchhait2007@gmail.com>
2026-09-22 18:36:54 +0530

    Refactor quick sort implementation and improve readability

--- 07-Sorting/21_quick_sort.c
+++ 07-Sorting/21_quick_sort.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
 
-static void swap(int *a, int *b)
-{
-    int temp = *a; *a = *b; *b = temp;
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-static int partition(int a[], int low, int high)
-{
-    int pivot = a[high];
-    int i = low - 1;
-
-    for (int j = low; j < high; ++j)
-        if (a[j] <= pivot) swap(&a[++i], &a[j]);
-
-    swap(&a[i + 1], &a[high]);
-    return i + 1;
+int partition(int array[], int low, int high) {
+    int pivot = array[high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++) {
+        if (array[j] <= pivot) {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
 }
 
-static void quick_sort(int a[], int low, int high)
-{
+void quickSort(int array[], int low, int high) {
     if (low < high) {
-        int pivot_index = partition(a, low, high);
-        quick_sort(a, low, pivot_index - 1);
-        quick_sort(a, pivot_index + 1, high);
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
     }
 }
 
-int main(void)
-{
-    int a[] = {10, 7, 8, 9, 1, 5};
-    int n = (int)(sizeof(a) / sizeof(a[0]));
-
-    printf("Unsorted array: ");
-    for (int i = 0; i < n; ++i) printf("%d%s", a[i], i + 1 == n ? "\n" : " ");
-
-    quick_sort(a, 0, n - 1);
-
-    printf("Sorted array: ");
-    for (int i = 0; i < n; ++i) printf("%d%s", a[i], i + 1 == n ? "\n" : " ");
+int main() {
+    int data[] = {6, 4, 7, 8, 1, 9, 5};
+    int size = sizeof(data) / sizeof(data[0]);
+    
+    quickSort(data, 0, size - 1);
+    
+    printf("Sorted Array\n");
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
     return 0;
 }
