c0e48a3e77b691cc761f6730fe70e65657c508d0
Srijani-Guchhait <srijaniguchhait2007@gmail.com>
2026-09-22 18:27:38 +0530

    Replace Selection Sort with Insertion Sort implementation

--- 07-Sorting/17_insertion_sort.c
+++ 07-Sorting/17_insertion_sort.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
-void SelectionSort(int array[], int size) {
-    for (int step = 0; step < size - 1; step++) {
-        int min_idx = step;
-        for (int i = step + 1; i < size; i++) {
-            if (array[i] < array[min_idx]) {
-                min_idx = i;
-            }
+void insertionSort(int arr[], int size) {
+    for (int step = 1; step < size; step++) {
+        int key = arr[step];
+        int j = step - 1;
+
+        while (j >= 0 && key < arr[j]) {
+            arr[j + 1] = arr[j];
+            --j;
         }
-        int temp = array[min_idx];
-        array[min_idx] = array[step];
-        array[step] = temp;
+        arr[j + 1] = key;
     }
 }
 
 int main() {
     int data[] = {6, 4, 7, 8, 1, 9, 5};
     int size = sizeof(data) / sizeof(data[0]);
-    
-    SelectionSort(data, size);
-    
+
+    insertionSort(data, size);
+
     printf("Sorted Array\n");
     for (int i = 0; i < size; ++i) {
         printf("%d ", data[i]);
     }
-    printf("\n");
+
     return 0;
 }
