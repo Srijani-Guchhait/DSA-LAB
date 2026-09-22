6b01e52e0fcc38b47b6360f67ffa87e6be906859
Srijani-Guchhait <srijaniguchhait2007@gmail.com>
2026-09-22 18:51:33 +0530

    Improve factorial function and input handling
    Refactor factorial function and main for clarity.

--- 01-Recursion/01_factorial_recursion.c
+++ 01-Recursion/01_factorial_recursion.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 
-long long factorial(int n)
-{
-    if (n <= 1)
-        return 1;
-    return n * factorial(n - 1);
-}
-
-int main(void)
-{
-    int number;
-
-    printf("Enter a non-negative integer: ");
-    if (scanf("%d", &number) != 1 || number < 0 || number > 20) {
-        fprintf(stderr, "Invalid input. Enter a value from 0 to 20.\n");
+int factorial(int n) {
+    if (n == 0 || n == 1) {
         return 1;
     }
+    else
+        return (n * factorial(n - 1));
+}
 
-    printf("Factorial of %d = %lld\n", number, factorial(number));
+int main() {
+    int number;
+    printf("Enter the number: ");
+    scanf("%d", &number);
+    printf("Factorial of %d is %d", number, factorial(number));
+
     return 0;
 }
