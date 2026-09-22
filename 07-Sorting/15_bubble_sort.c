da6273cda4358312113a8393eedfcbb6d0a72e3e
Srijani-Guchhait <srijaniguchhait2007@gmail.com>
2026-09-22 18:32:22 +0530

    Refactor bubble sort function and update main

--- 07-Sorting/15_bubble_sort.c
+++ 07-Sorting/15_bubble_sort.c
@@ -1,23 +1,24 @@
-#include <stdio.h>
+#include<stdio.h>
 
-static void bubble_sort(int a[], int n)
-{
-    for (int pass = 0; pass < n - 1; ++pass)
-        for (int j = 0; j < n - pass - 1; ++j)
-            if (a[j] > a[j + 1]) {
-                int temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
+void bubbleSort(int arr[], int size) {
+    for (int step = 0; step < size - 1; step++) {
+        for (int j = 0; j < size - step - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
+        }
+    }
 }
 
-int main(void)
-{
-    int a[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = (int)(sizeof(a) / sizeof(a[0]));
-
-    bubble_sort(a, n);
+int main() {
+    int data[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(data) / sizeof(data[0]);
+    bubbleSort(data, size);
     printf("Sorted array: ");
-    for (int i = 0; i < n; ++i) printf("%d%s", a[i], i + 1 == n ? "\n" : " ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", data[i]);
+    }
     return 0;
 }
